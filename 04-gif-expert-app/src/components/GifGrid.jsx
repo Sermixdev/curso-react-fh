74b54e444d9c7833d6b50bde5022c5aabb4bcdf3
Sermixdev <sermixdev@gmail.com>
2023-11-15 19:47:14 +0100

    Fin sección 6 - Archivo barril o index

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,24 +1,18 @@
-import { useEffect, useState } from 'react';
-
-import { getGifs } from '../helpers/getGifs';
 import { GifItem } from './GifItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([]);
+    
+    const { images, isLoading} = useFetchGifs( category );
+    
 
-    const getImages = async () => {
-        const newImages = await getGifs(category);
-        console.log(newImages);
-        setImages(newImages);
-    };
-
-    useEffect(() => {
-        getImages();
-    }, []);
 
     return (
         <>
             <h3>{category}</h3>
+            {
+                isLoading &&  (<h2>Cargando...</h2>)
+            }
             <div className='card-grid'>
                 {images.map((image) => (
                     <GifItem key={image.id} {...image} />
