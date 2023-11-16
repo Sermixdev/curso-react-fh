791489dc37018f34a6889b914a9b66b56c5152fd
Sermixdev <sermixdev@gmail.com>
2023-11-16 20:32:11 +0100

    Test suites con AddCategory + GifGrid

---
+++ 04-gif-expert-app/src/tests/helpers/getGifs.test.js
@@ -0,0 +1,14 @@
+import { getGifs } from '../../helpers/getGifs';
+
+describe('Pruebas en getGifs', () => {
+    test('should return gif arrays', async () => {
+        const gifs = await getGifs('One Punch');
+        // console.log(gifs)
+        expect(gifs.length).toBeGreaterThan(0);
+        expect(gifs[0]).toEqual({
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String),
+        });
+    });
+});
