8dbf5ecf6769dad9666b7b947ac20e6a5e4d1be3
Sermixdev <sermixdev@gmail.com>
2023-11-22 23:49:30 +0100

    Repaso antes del useReducer

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
@@ -12,10 +11,14 @@ import './index.css'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre';
+// import './08-useReducer/intro-reducer';
+import { TodoApp } from './08-useReducer/TodoApp';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>,
-)
+    //   // <React.StrictMode>
+    <TodoApp />
+    //   // </React.StrictMode>,
+);
