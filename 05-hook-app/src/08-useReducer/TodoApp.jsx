07580c3b60d5c35ca4de27066d8bf338a61d7428
Sermixdev <sermixdev@gmail.com>
2023-11-23 23:27:49 +0100

    Finaliza todo hooks

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,49 +1,19 @@
-import { useEffect, useReducer } from 'react';
-import { todoReducer } from './todoReducer';
 import { TodoList } from './TodoList';
 import { TodoAdd } from './TodoAdd';
-
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del Alma',
-    //     done: false,
-    // },
-];
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || [];
-};
+import { useTodos } from '../hooks';
 
 export const TodoApp = () => {
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
-
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] Add todo',
-            payload: todo,
-        };
-        dispatch(action);
-    };
-    const handleDeleteTodo = (id) => {
-        dispatch({
-            type: '[TODO] Remove todo',
-            payload: id,
-        });
-    };
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleNewTodo, handleToggleTodo } = useTodos();
     return (
         <>
             <h1>
-                TodoApp: 10, <small>pendientes: 2</small>
+                TodoApp: {todosCount} <small>pendientes: {pendingTodosCount}</small>
             </h1>
             <hr />
 
             <div className='row'>
                 <div className='col-7'>
-                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} onToggleTodo={handleToggleTodo} />
                 </div>
                 <div className='col-5'>
                     <h4>Agregar TODO</h4>
